ae3a91b7ee9b697f48491c599eaea14c97af1597
Bruno Baère Pedrazzi Lomba de Araujo <killerasus@gmail.com>
2021-04-24 02:35:00 -0300

    Controls sections based on navbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+var sections = [];
 
+document.addEventListener("DOMContentLoaded", function(){
+    // add padding top to show content behind navbar
+    navbar_height = document.querySelector('.navbar').offsetHeight;
+    document.body.style.paddingTop = navbar_height + 'px';
+
+    sections["contacts"] = document.getElementById("contacts");
+    sections["research"] = document.getElementById("research");
+    sections["games"] = document.getElementById("games");
+    sections["rpg"] = document.getElementById("rpg");
+
+    showOnly("contacts");
+  }); 
+
+  function showOnly(sectionId) {
+      for (let section of Object.values(sections)) {
+        if(section.id === sectionId)
+          section.style.display = "inline";
+        else
+          section.style.display = "none";
+      }
+  }

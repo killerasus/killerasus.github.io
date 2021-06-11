0b6ead1eff9f2db5d359009e1b1a78949439abcc
Bruno Baère Pedrazzi Lomba de Araujo <killerasus@gmail.com>
2021-06-11 03:22:50 -0300

    Updates cv and short resume

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var sections = [];
+var nav_items = [];
 
 document.addEventListener("DOMContentLoaded", function(){
     // add padding top to show content behind navbar
@@ -10,14 +11,25 @@ document.addEventListener("DOMContentLoaded", function(){
     sections["games"] = document.getElementById("games");
     sections["rpg"] = document.getElementById("rpg");
 
+    // nav_items["contacts"] = document.getElementById("nav-contacts");
+    // nav_items["research"] = document.getElementById("nav-research");
+    // nav_items["games"] = document.getElementById("nav-games");
+    // nav_items["rpg"] = document.getElementById("nav-rpg");
+
     showOnly("contacts");
   }); 
 
   function showOnly(sectionId) {
       for (let section of Object.values(sections)) {
         if(section.id === sectionId)
+        {
           section.style.display = "inline";
+          // nav_items[section.id].addClass("active");
+        }
         else
+        {
           section.style.display = "none";
+          // nav_items[section.id].removeClass("active");
+        }
       }
   }
